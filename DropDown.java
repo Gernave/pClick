5e9fc6d64109ffb4c31818a6f556751e3c2954dd
Gernave <micpmd95@gmail.com>
2017-04-06 22:31:58 -0500

    Add files via upload

--- DropDown.java
+++ DropDown.java
@@ -1,42 +1,88 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class DropDown{
 
-	JFrame f;
-	JFrame BarChart;
+	JFrame f;//main window
+	JFrame BarChart;//window displaying barchart
 
 	DropDown(){    
 		f = new JFrame("Clicker");
+		
+		/*
+		 * Top buttons
+		 */
+		JPanel topButtons = new JPanel();
+		topButtons.setLayout(new FlowLayout());
+		//panel.add(topButtons, BorderLayout.PAGE_START);
+		
+		JButton updateB = new JButton("Update Database");
+		JButton helpB = new JButton("Help");
+		
+		topButtons.add(updateB);
+		topButtons.add(helpB);
+		
+		
+		/*
+		 * Labels for selection menus
+		 */
+		JPanel labels = new JPanel();
+		labels.setLayout(new BoxLayout(labels, BoxLayout.PAGE_AXIS));
+		
+		JLabel Term = new JLabel("Term");
+		JLabel Class = new JLabel("Class");
+		labels.add(Term);
+		labels.add(Class);
+		
+		/*
+		 * Section of selection menus
+		 */
+		JPanel dropdowns = new JPanel();
+		dropdowns.setLayout(new BoxLayout(dropdowns, BoxLayout.PAGE_AXIS));
 
-		JPanel panel = new JPanel();//uses BorderLayout by default
-		f.add(panel);
-
-		TextArea text = new TextArea(20,50);
-
-		JButton button=new JButton("Show");
-
-		String choices[] = { "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
-		JComboBox cb = new JComboBox(choices);
-
-		panel.add(cb, BorderLayout.PAGE_START);
-		panel.add(text, BorderLayout.CENTER);
-		panel.add(button, BorderLayout.PAGE_END); 
-
-		f.setSize(400,500);    
+		JButton button = new JButton("Go");//takes user input
+		
+		String choices[] = {"Term", "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+		String choices2[] = {"Class", "CHOICE 1","CHOICE 2", "CHOICE 3","CHOICE 4","CHOICE 5","CHOICE 6"};
+		JComboBox cb = new JComboBox(choices);//drop-down menu
+		JComboBox cb2 = new JComboBox(choices2);
+		
+		dropdowns.add(new JLabel("Search"));
+		dropdowns.add(cb);
+		dropdowns.add(cb2);
+		dropdowns.add(button);
+		
+		/*
+		 * Section of results
+		 */
+		TextArea text = new TextArea(20,50);//prints lists of classes, answers etc.
+		f.getContentPane().add(topButtons, BorderLayout.PAGE_START);
+		//f.getContentPane().add(labels, BorderLayout.LINE_START);
+		f.getContentPane().add(dropdowns, BorderLayout.CENTER);
+		f.getContentPane().add(new JLabel("Results"),BorderLayout.PAGE_END);
+		f.getContentPane().add(text, BorderLayout.PAGE_END);
+		
+		f.pack();
+		
 		f.setVisible(true);
 		
-		
+		/*
+		 * Window which opens to display bar-chart 
+		 */
 		BarChart = new JFrame("Bar Chart");
 		
 		ImageIcon image = new ImageIcon("C:\\Users\\Gernave\\Pictures\\Screenshots\\Screenshot (5).png");
         JLabel imageLabel = new JLabel(image); 
         BarChart.add(imageLabel);
-		
-		BarChart.setSize(400,500);    
+	
+		BarChart.pack();
 		BarChart.setVisible(false);
 
+		/*
+		 * Does things depending on choice of drop-down menu's option
+		 */
 		ActionListener a = new ActionListener() {  
 			public void actionPerformed(ActionEvent e) {
 				if(cb.getItemAt(cb.getSelectedIndex()).equals("CHOICE 1")){
@@ -60,6 +106,8 @@ public class DropDown{
 
 
 	public static void main(String[] args) {    
-		new DropDown();         
+		new DropDown(); 
+		//new ThreeCascadeJComboBox();
+		//new GUI2();
 	} 
 }
