7fee50deaa93a50774473db05c1e52addf6baf8d
Gernave <micpmd95@gmail.com>
2017-04-13 21:43:34 -0500

    Add files via upload

--- GUI2.java
+++ GUI2.java
@@ -5,51 +5,147 @@ import java.awt.event.*;
 
 public class GUI2{
 
-	private JFrame f = new JFrame("Clicker");//main window
+	private JFrame f = new JFrame("Title");//main window
 	
 	private JFrame BarChart = new JFrame("Bar Chart");//window displaying barchart
 	
-	private JButton updateB = new JButton("Show picture");
+	private JButton updateB = new JButton("Update data for your classes");
 	
 	private JButton helpB = new JButton("Help");
 
-	private JButton button = new JButton("Search");//takes user input
+	private JButton button = new JButton("Go");//takes user input
 	
 	GUI2(){
 		JPanel topButtons = top();
 		JPanel dropdowns = center();
-		
-		//options in drop down menu
-		String choices[] = {"Term", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		String classList[] = {"Class", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		String sessionList[] = {"Session", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		String showOptions[] = {"Show", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		String ignoreOptions[] = {"Ignore", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		String orderOptions[] = {"Order by", "CHOICE 1","CHOICE 2", "CHOICE 3"};
-		
-		JComboBox cb = makeDropDown(choices);
-		JComboBox classMenu = makeDropDown(classList);
-		JComboBox sessionMenu = makeDropDown(sessionList);
-		JComboBox showMenu = makeDropDown(showOptions);
-		JComboBox ignoreMenu = makeDropDown(ignoreOptions);
-		JComboBox orderMenu = makeDropDown(orderOptions);
-				
-		dropdowns.add(cb);		
-		dropdowns.add(classMenu);		
-		dropdowns.add(sessionMenu);		
-		dropdowns.add(showMenu);		
-		dropdowns.add(ignoreMenu);
-		dropdowns.add(orderMenu);
-		
-		//showImage(/*insert image address here to display it in new window*/);
-		
-		
-		
-		/*
-		 * Section of results
-		 */
 		JPanel results = new JPanel();
 		results.setLayout(new BoxLayout(results, BoxLayout.PAGE_AXIS));
+		
+		//options in drop down menu
+		
+		JPanel labelColumn = new JPanel();
+		JPanel dropdownColumn = new JPanel();
+		JPanel otherColumn = new JPanel();
+		
+		dropdowns.add(labelColumn);
+		dropdowns.add(dropdownColumn);
+		dropdowns.add(otherColumn);
+		
+		labelColumn.setLayout(new BoxLayout(labelColumn, BoxLayout.PAGE_AXIS));
+		dropdownColumn.setLayout(new BoxLayout(dropdownColumn, BoxLayout.PAGE_AXIS));
+		otherColumn.setLayout(new BoxLayout(otherColumn, BoxLayout.PAGE_AXIS));
+
+		
+					//****LABELS****//
+		
+		
+		JPanel labelPanel_1 = new JPanel();
+		JPanel labelPanel_2 = new JPanel();
+		JPanel labelPanel_3 = new JPanel();
+		JPanel labelPanel_4 = new JPanel();
+		JPanel labelPanel_5 = new JPanel();
+		JPanel labelPanel_6 = new JPanel();
+		
+		JLabel label_1 = new JLabel("Term");
+		JLabel label_2 = new JLabel("Class");
+		JLabel label_3 = new JLabel("Session");
+		JLabel label_4 = new JLabel("Filter");
+		JLabel label_5 = new JLabel("Order by");
+		JLabel label_6 = new JLabel("Ignore");
+				
+		labelPanel_1.add(label_1);
+		labelPanel_2.add(label_2);
+		labelPanel_3.add(label_3);
+		labelPanel_4.add(label_4);
+		labelPanel_5.add(label_5);
+		labelPanel_6.add(label_6);
+		
+		labelColumn.add(labelPanel_1);
+		labelColumn.add(labelPanel_2);
+		labelColumn.add(labelPanel_3);
+		labelColumn.add(labelPanel_4);
+		labelColumn.add(labelPanel_5);
+		labelColumn.add(labelPanel_6);
+		
+		labelColumn.add(Box.createVerticalStrut(55));
+		
+		
+					//****DROPDOWNS****//
+		
+		
+		String choices[] = {"CHOICE 1","CHOICE 2", "CHOICE 3"};
+		String classList[] = {"CHOICE 1","CHOICE 2", "CHOICE 3"};
+		String sessionList[] = {"CHOICE 1","CHOICE 2", "CHOICE 3"};
+		String showOptions[] = {"CHOICE 1","CHOICE 2", "CHOICE 3"};
+		//String ignoreOptions[] = {"Ignore", "CHOICE 1","CHOICE 2", "CHOICE 3"};
+		String orderOptions[] = {"Chronological", "CHOICE 1","CHOICE 2", "CHOICE 3"};
+		
+		JComboBox cb = new JComboBox(choices);
+		JComboBox classMenu = new JComboBox(classList);
+		JComboBox sessionMenu = new JComboBox(sessionList);
+		JComboBox showMenu = new JComboBox(showOptions);
+		//JComboBox ignoreMenu = new JComboBox(ignoreOptions);
+		JComboBox orderMenu = new JComboBox(orderOptions);
+		
+		JCheckBox checkBox_1 = new JCheckBox("Paired questions");
+		JCheckBox checkBox_2 = new JCheckBox("1st half of paired questions");
+		/*
+		JPanel ddPanel_1 = new JPanel();
+		JPanel ddPanel_2 = new JPanel();
+		JPanel ddPanel_3 = new JPanel();
+		JPanel ddPanel_4 = new JPanel();
+		JPanel ddPanel_5 = new JPanel();
+		JPanel ddPanel_6 = new JPanel();
+		
+		ddPanel_1.add(cb);
+		ddPanel_2.add(classMenu);		
+		ddPanel_3.add(sessionMenu);		
+		ddPanel_4.add(showMenu);		
+		ddPanel_5.add(ignoreMenu);
+		ddPanel_6.add(orderMenu);
+		
+		dropdownColumn.add(ddPanel_1);
+		dropdownColumn.add(ddPanel_2);		
+		dropdownColumn.add(ddPanel_3);		
+		dropdownColumn.add(ddPanel_4);		
+		dropdownColumn.add(ddPanel_5);
+		dropdownColumn.add(ddPanel_6);
+		*/
+
+		dropdownColumn.add(cb);
+		dropdownColumn.add(classMenu);		
+		dropdownColumn.add(sessionMenu);		
+		dropdownColumn.add(showMenu);
+		dropdownColumn.add(orderMenu);		
+		dropdownColumn.add(checkBox_1);		
+		dropdownColumn.add(checkBox_2);
+		dropdownColumn.add(button);				
+
+		
+		//****RIGHT COLUMN****//
+
+
+		JCheckBox checkBox_3 = new JCheckBox("Unpaired suestions");
+		JCheckBox checkBox_4 = new JCheckBox("2nd half of paired questions");
+		JRadioButton radioButton_1 = new JRadioButton("Ascending");
+		JRadioButton radioButton_2 = new JRadioButton("Descending");
+		
+		JPanel radioButtons = new JPanel();
+		
+		radioButtons.add(radioButton_1);
+		radioButtons.add(radioButton_2);
+
+		otherColumn.add(Box.createRigidArea(new Dimension(0,65)));
+		otherColumn.add(radioButtons);
+		otherColumn.add(checkBox_3);
+		otherColumn.add(checkBox_4);
+		
+		radioButtons.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		
+		//****RESULTS****//
+		
+		
 		TextArea text = new TextArea(3,50);//prints lists of classes, answers etc.
 		TextArea text2 = new TextArea(5,50);//prints lists of classes, answers etc.
 		TextArea text3 = new TextArea(5,50);//prints lists of classes, answers etc.
@@ -129,13 +225,8 @@ public class GUI2{
 		 * Section of selection menus
 		 */
 		JPanel dropdowns = new JPanel();
-		dropdowns.setLayout(new BoxLayout(dropdowns, BoxLayout.PAGE_AXIS));
-		dropdowns.add(button);
+		//dropdowns.setLayout(new BoxLayout(dropdowns, BoxLayout.PAGE_AXIS));
+		dropdowns.setLayout(new FlowLayout());
 		return dropdowns;
-	}
-	
-	private JComboBox makeDropDown(String choices[]){
-		JComboBox cb = new JComboBox(choices);//drop-down menu
-		return cb;
 	}
 }
